65936c9a929e877775c133ad54ec9a2d5b05edba
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 21:38:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 # Write your code here.
 def line(array)
-  puts "The line is currently empty." if array.size == 0
-  number = 0
-  current_place = []
-  array.each do |name|
-    number += 1
-    current_place << "#{number}. #{name}"
-    puts current_place
+  if array.size == 0
+    puts "The line is currently empty." 
+  else
+    number = 0
+    current_place = []
+    array.each do |name|
+      number += 1
+      current_place << ("#{number}. #{name}")
+    end
   end
 end
