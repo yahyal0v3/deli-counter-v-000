ca48a7f99c85ee57784ad2355b7297d21f08bd06
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 22:47:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,7 @@ end
 def take_a_number(array, name)
   array << name
   number = array.index(name) + 1
-    array.each do |name|
-      puts "Welcome, #{name}. You are number #{number} in line."
-    end
+  "Welcome, #{name}. You are number #{number} in line."
 end
 
 #number = 0
