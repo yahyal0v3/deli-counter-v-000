70682496bca385a168d2a6d18a26da488e0693d9
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-09-30 20:32:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,13 @@
 # Write your code here.
 def line(array)
   puts "The line is currently empty." if array.size == 0
+  number = 0
+  current_place = []
+  array.collect do |name|
+    number += 1
+    current_place = "#{number}. " + "#{name}"
+  end
+  current_place
 end
+
+
